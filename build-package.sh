3db4a037b57e10513dc171ab357074181758d283
fsatrion <62125821+fsatrion@users.noreply.github.com>
2021-06-08 07:23:00 +0700

    Update build-package.sh

--- build-package.sh
+++ build-package.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-apt-get install -y install apache2 php wget unzip libapache2-mod-php php-{gd,common,mail,mail-mime,mysql,pear,db,mbstring,xml,curl}
+apt install -y install apache2 php wget libapache2-mod-php php-{gd,common,mail,mail-mime,mysql,pear,db,mbstring,xml,curl}
 
 apt install -y software-properties-common
 apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
 add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mariadb.mirror.liquidtelecom.com/repo/10.5/ubuntu bionic main'
 apt update
-apt install mariadb-server
+apt install -y mariadb-server
+apt install -y unzip
 
 sudo mysql_secure_installation
 
-mysql -u base_user -pbase_user_pass -e "create database radius; GRANT ALL PRIVILEGES ON new_db.* TO radius@localhost IDENTIFIED BY 'b4kso3nak'; FLUSH PRIVILEGES; QUIT"
+mysql -u root -p -e "create database radius; GRANT ALL PRIVILEGES ON new_db.* TO radius@localhost IDENTIFIED BY 'b4kso3nak'; FLUSH PRIVILEGES"
 
 apt -y install freeradius freeradius-mysql freeradius-utils
-mysql -u root -p radius < /etc/freeradius/3.0/mods-config/sql/main/mysql/schema.sq
+mysql -u root -p radius < /etc/freeradius/3.0/mods-config/sql/main/mysql/schema.sql
 ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled/
 
 nano /etc/freeradius/3.0/mods-enabled/sql
