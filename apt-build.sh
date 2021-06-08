cbbedf849264987f3678670e84e78f29dfec7e1c
fsatrion <62125821+fsatrion@users.noreply.github.com>
2021-06-08 09:37:10 +0700

    Update apt-build.sh

--- apt-build.sh
+++ apt-build.sh
@@ -9,11 +9,12 @@ apt update
 apt install -y mariadb-server
 
 sudo mysql_secure_installation
-
+echo
 echo -e "Enter your sql password"
 mysql -u root -p -e "create database db_name; GRANT ALL PRIVILEGES ON db_name.* TO new_db_user@localhost IDENTIFIED BY 'db_user_pass'; FLUSH PRIVILEGES"
 
 apt -y install freeradius freeradius-mysql freeradius-utils
+echo
 echo -e "Enter your sql password"
 mysql -u root -p radius < /etc/freeradius/3.0/mods-config/sql/main/mysql/schema.sql
 ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled/
@@ -29,8 +30,10 @@ wget https://github.com/lirantal/daloradius/archive/master.zip
 unzip master.zip
 mv daloradius-master daloradius
 cd daloradius
+echo
 echo -e "Enter your sql password"
 mysql -u root -p radius < /root/Daloradius-Radiusv3/db/fr3-mysql-daloradius-and-freeradius.sql
+echo
 echo -e "Enter your sql password"
 mysql -u root -p radius < contrib/db/mysql-daloradius.sql
 cd -
