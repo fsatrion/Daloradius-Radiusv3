9e613b4483a4608e8da663fa5ee68e3aa1f7282d
fsatrion <62125821+fsatrion@users.noreply.github.com>
2021-06-08 09:10:45 +0700

    Centos7

--- yum-build.sh
+++ yum-build.sh
@@ -1,9 +1,19 @@
-# APACHE install
+#!/bin/bash
 yum -y update
+setenforce 0
+sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
+
 yum -y install groupinstall "Developer-Tools"
 yum -y install httpd httpd-devel
+yum -y install wget unzip nano
 
-#MariaDB Install
+yum -y install epel-release
+yum -y install http://rpm.remirepo.net/enteeprise/remi-release-7.rpm
+yum -y insyall yum-utils
+yum-config-manager --disable remi-php54
+yum-config-manager --enable remi-php72
+yum -y insyall php php-{cli,curl,mysqlnd,devel,gd,pear,mcrypt,mbstring,xml,pear}
+
 sudo tee /etc/yum.repo.d/MariaDB.repo<<EOOF
 [mariadb]
 name = mariaDB
@@ -11,33 +21,40 @@ baseurl = http://yum.mariadb.org/10.4/centos7-amd64
 gpgkey = https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck = 1
 EOF
+yum -y install MariaDB-server
 systemctl enable mariadb
 systemctl start mariadb
 
-#SQL Config
 mysql _secure_installation
 
-#Databse Config
-mysql -u root -p
-CREATE DATABASE radius;
-GRANT ALL ON radius.* TO radius@localhost IDENTIFED BY "123";
-FLUSH PRIVILEGES;
-\q
+mysql -u root -p -e "create database db_name; GRANT ALL PRIVILEGES ON db_name.* TO new_db_user@localhost IDENTIFIED BY 'db_user_pass'; FLUSH PRIVILEGES"
 
-#PHP Install
-yum -y install epel-release
-yum -y install http://rpm.remirepo.net/enteeprise/remi-release-7.rpm
-yum -y insyall yum-utils
-yum-config-manager --disable remi-php54
-yum-config-manager --enable remi-php72
-yum -y insyall php php-{cli,curl,mysqlnd,devel,gd,pear,mcrypt,mbstring,xml,pear}
-php -v
+yum -y install freeradius freeradius-utils freeradius-mysql
+systemctl enable --now radiusd.service
+firewall-cmd --add-service={http,https,radius} --permanent
+firewall-cmd --reload
 
-#PHPMyAdmin install
-yum -y install phpmyadmin
+mysql -u root -p radius < /etc/raddb/mods-config/sql/main/mysql/schema.sql
+ln -s /etc/raddb/mods-available/sql /etc/raddb/mods-enabled/
 
-#wget install
-yum -y install wget
+nano /etc/raddb/mods-available/sql
+chgrp -h radiusd /etc/raddb/mods-enabled/sql
 
-#nano install
-yum -y install nano
+wget https://github.com/lirantal/daloradius/archive/master.zip
+unzip master.zip
+mv daloradius-master/ daloradius
+cd daloradius
+
+mysql -u root -p radius < /root/Daloradius-Radiusv3/db/fr3-mysql-daloradius-and-freeradius.sql
+mysql -u root -p radius < contrib/db/mysql-daloradius.sql
+cd -
+mv daloradius /var/www/html/
+
+chown -R apache:apache /var/www/html/daloradius/
+chmod 664 /var/www/html/daloradius/library/daloradius.conf.php
+
+nano /var/www/html/daloradius/library/daloradius.conf.php
+
+systemctl restart radiusd.service
+systemctl restart httpd
+systemctl restart mariadb
