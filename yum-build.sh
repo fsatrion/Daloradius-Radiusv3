4d6d316e45bc94348c5e453975d04d04e8aba66b
fsatrion <62125821+fsatrion@users.noreply.github.com>
2021-06-08 09:26:21 +0700

    Update yum-build.sh

--- yum-build.sh
+++ yum-build.sh
@@ -20,8 +20,7 @@ name = mariaDB
 baseurl = http://yum.mariadb.org/10.4/centos7-amd64
 gpgkey = https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck = 1
-EOF
-echo
+EOF>>
 
 yum -y install MariaDB-server
 systemctl start mariadb
